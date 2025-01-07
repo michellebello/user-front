216480db4c711fcadd94592c7ac455229879d01f
michellebello <michellebelloveramendi@gmail.com>
2025-01-07 17:29:18 -0500

    added log in component

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,7 +2,19 @@ import React from "react";
 // import axios from "axios";
 
 function Login() {
-  return <h1>Welcome back!</h1>;
+  return (
+    <div>
+      <h1>Welcome back!</h1>
+      <div>
+        <label for="username">Username</label>
+        <input type="text"></input>
+      </div>
+      <div>
+        <label for="password">Password</label>
+        <input type="text"></input>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
